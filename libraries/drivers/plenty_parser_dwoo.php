08aa253375c982f469aafad70c9d7e5563fa375b
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-04-18 15:12:39 +1000

    More barebones drivers added which will be filled with gooey goodness shortly.

---
+++ libraries/drivers/plenty_parser_dwoo.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Plenty_parser_dwoo extends CI_Driver {
+	
+	
+
+}
---
+++ libraries/drivers/plenty_parser_twig.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Plenty_parser_twig extends CI_Driver {
+	
+	
+
+}
